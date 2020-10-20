dd25f8cb3d02dee7d6b9767f6f7d76a2ffbc69b3
Techspawn Solution <siddharth@techspawn.com>
2020-10-20 10:35:16 +0530

    [ADD] sku_code.py

--- ledpax/models/__init__.py
+++ ledpax/models/__init__.py
@@ -4,7 +4,7 @@
 # from . import stock_rule
 # from . import related_products
 from . import bom
-# from . import sku_code
+from . import sku_code
 # from . import sku_code_prod
 # from . import quatation
 # from . import order_tag

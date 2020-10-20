d8a3391fb7f0abadc87c672ead8b8ffdda357dd0
Techspawn Solution <siddharth@techspawn.com>
2020-10-20 18:21:45 +0530

    [ADD]change in code

--- ledpax/controllers/__init__.py
+++ ledpax/controllers/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from . import controllers
-from . import purchase_order
+# from . import purchase_order
 from . import vendor_data_file
-# from . import order_tag
+from . import order_tag
